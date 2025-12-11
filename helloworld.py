1f66b58d3b1784196e2c7eb0ec42b77ce31fb6be
sachin321888 <35649054+sachin321888@users.noreply.github.com>
2025-12-11 22:10:47 +0530

    just1

--- helloworld.py
+++ helloworld.py
@@ -1,2 +1,7 @@
-
+print("Hi sachin)
 print("Hello world")
+original_string = "hello world"
+reversed_string = original_string[::-1]
+
+print(f"Original: {original_string}")
+print(f"Reversed: {reversed_string}")
